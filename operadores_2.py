e1dbac814990876d96a36bb76135a38bd9080e7d
unknown <meteoro5andres@hotmail.com>
2023-06-09 17:00:14 -0600

    05 operadores 2

---
+++ operadores_2.py
@@ -0,0 +1,18 @@
+# operadores boleanos
+# == (igualdad)
+print(1==1)
+# != (distinto)
+print(1 != 1)
+# > (Mayor que)
+print (1>1)
+# < (menor que)
+print(1<1)
+#Operadores logicos 
+#and
+print(True and True)
+print(True and True)
+# or 
+print(True or True)
+print(False and False)
+# not 
+print(not True)
