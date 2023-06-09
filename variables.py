fda57834d9ce9e7b29a742b0aa13c60cb967022d
unknown <meteoro5andres@hotmail.com>
2023-06-09 16:24:20 -0600

    02 variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+
+
+name = "Luis"
+last_name = "Medel"
+age = 18
+height = 1.90
+is_student = True 
+groups = ["3C"]
+
+ #function type
+
+print(type (name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
