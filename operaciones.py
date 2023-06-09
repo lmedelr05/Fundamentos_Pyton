4dd1bb79f457a1ddbe89ce56ccab8abfbef1fcb6
unknown <meteoro5andres@hotmail.com>
2023-06-09 16:53:47 -0600

    04 operadores

---
+++ operaciones.py
@@ -0,0 +1,19 @@
+# operadores matematicos 
+# " + - * %"
+# /División float
+# /División entera
+# ** potencia 
+print(12 + 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 3)
+print(2 ** 16)
+
+# Operadores para strings 
+# + para concatenar
+# * para replicar 
+print("_______" * 4)
+print("hola " + "mundo!")
+print("Hola " * 3)
